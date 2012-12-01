701798e8c39544880b40b876bb63ba58dc152fc9
ycyrffgirlie <ycyrffgroupie@gmail.com>
2012-12-01 11:27:45 +0000

    The ew fuction

--- includes/new.php
+++ includes/new.php
@@ -1,23 +1,62 @@
 <?php
+
+//$connection = mysql_connect("localhost","ycyrf718_ycyrff","y1986Cyrff") or die(mysql_error());
+//mysql_select_db("ycyrf718_cyrff", $connection) or die(mysql_error());
+function whatnewen(){
 if (file_exists('/includes/connection.php')){
 	require('includes/connection.php');
 }else{
 	require("connection.php");
 }
+	$sql = 'SELECT `filename`, `title`, `whatnewenglish`, `datelastmodified`, `includedinnew`, `newsortorder`FROM `pages` 
+	WHERE `includedinnew`= 1
+	ORDER BY `newsortorder` DESC';
+	$query = mysql_query($sql);
+	
+//print_r($newenresults);
+	
+	$output = '<p>What is new?'."\n";
+	$output .= '<br>'."\n";
+	$output .= '<br>'."\n";
+	
 
+	while($newenresults = mysql_fetch_assoc($query)){
+		$output .= '<a class="normal" href="'.$newenresults['filename'].'">'.$newenresults['title'].'</a> - '.$newenresults['whatnewenglish']."\n";
+		$output .='<br>'."\n";
+		
+	}
 
-//$connection = mysql_connect("localhost","ycyrf718_ycyrff","y1986Cyrff") or die(mysql_error());
-//mysql_select_db("ycyrf718_cyrff", $connection) or die(mysql_error());
-function whatnewen(){
+	
+	mysql_close($connection);
 
-$sql = 'SELECT `filename`, `title`, `whatnewenglish`, `datelastmodified`, `includedinnew` FROM `pages` WHERE `includedinnew`= 1';
-$query = mysql_query($sql);
-$newenresults = mysql_fetch_assoc($query);
-print_r($newenresults);
-/*foreach ($newenresults as $newpage){
-	print_r($newpage);
-}*/
-return $newenresults;
+	return $output;
 }
-mysql_close($connection);
+
+function whatnewcy(){
+if (file_exists('/includes/connection.php')){
+	require('includes/connection.php');
+}else{
+	require("connection.php");
+}
+	$sql = 'SELECT `filename`, `title`, `whatnewcymraeg`, `datelastmodified`, `includedinnew`, `newsortorder` FROM `pages` 
+	WHERE `includedinnew`= 1
+	ORDER BY `newsortorder` DESC';
+	$query = mysql_query($sql);
+//print_r($newenresults);
+
+	$output = '<p>Beth sy newydd?'."\n";
+	$output .= '<br>'."\n";
+	$output .= '<br>'."\n";
+	while($newenresults = mysql_fetch_assoc($query)){
+		$output .= '<a class="normal" href="'.$newenresults['filename'].'">'.$newenresults['title'].'</a> - '.$newenresults['whatnewcymraeg']."\n";
+		$output .='<br>'."\n";
+	}
+	
+	mysql_close($connection);
+
+	return $output;
+
+
+}
+
 ?>
